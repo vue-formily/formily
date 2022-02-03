ad5b68a533bdac489c66b966be9d4ae76f37fa35
an ha <haan.an@yahoo.com.vn>
2022-02-03 09:43:02 +0700

    fix: correct import path

--- src/index.ts
+++ src/index.ts
@@ -121,7 +121,7 @@ export type {
   CollectionInstance,
   CollectionItemInstance
 } from './core/elements/instanceTypes';
-export { Validator, RuleSchema, ValidationOptions, ValidationRuleSchema } from './core/validations/types';
+export type { Validator, RuleSchema, ValidationOptions, ValidationRuleSchema } from './core/validations/types';
 export type { EventHandler, EventOptions, EventElement } from './core/Evento';
 
 export * from './core/validations';
